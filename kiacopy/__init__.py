63ebd92550bbcf553a40fe9ed22918a21111147d
ganariya <willwill1997willwill@yahoo.co.jp>
2020-07-19 10:04:49 +0900

    これまでの内容を踏まえて修正

--- kiacopy/__init__.py
+++ kiacopy/__init__.py
@@ -1,4 +1,12 @@
-"""Top-level package for kiacopy."""
+from .colony import Colony
+from .solver import Solver
+from .solverplugin import SolverPlugin
+from .solution import Solution
+from .ants.ant import Ant
+from .ants.randomant import RandomAnt
+from .ants.sensitiveant import SensitiveAnt
+from . import plugins
+from . import utils
 
 __author__ = """ganariya"""
 __email__ = 'ganariya2525@gmail.com'
